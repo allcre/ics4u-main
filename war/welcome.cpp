71947f2a26ece8eb1f87182644c714e6848d7432
Allison <allisoncretel@gmail.com>
2022-09-14 20:45:49 -0400

    k

--- war/welcome.cpp
+++ war/welcome.cpp
@@ -21,20 +21,18 @@ int welcome() {
 
     cout << "\nWelcome to the game of war! \n" << endl;
   
+    cout << "Would you like to read the instructions? (y or n)" << endl;
     string yn = "a"; // temp filler 
 
     // loop to print instructions 
     while (yn != "y" || "n") {
-      cout << "Would you like to read the instructions? (y or n)" << endl;
       cin >> yn;
       
       if (yn == "y") {
         instructions();
-        return 0;
       }
-      else if (yn == "n") {
+      else if (yn == "n")
         return 0;
-      }
       else 
         cout << "That's not a valid option, please try again." <<  endl;
     }
