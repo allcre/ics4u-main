8fc2ec8f2ed63fc481d5c3556596691c137f9749
Allison <allisoncretel@gmail.com>
2022-10-20 14:20:53 -0400

    q2

--- cboj/105.cpp
+++ cboj/105.cpp
@@ -1,6 +1,6 @@
 // Allison Cretel 
 // ICS4U-02
-// 15/10/22
+// 20/10/22
  
 #include <iostream>
 using namespace std;
@@ -28,9 +28,9 @@ void q1a() {
     } 
 
     for (int k = 0; k < row; k++) {
-        cout << "\n";
         for (int l = 0; l < column; l++) 
             cout << matrix[k][l] << ' ';
+        cout << "\n";
     }
 
     for (int m = 0; m < row; m++)
@@ -39,7 +39,47 @@ void q1a() {
     delete[] matrix;
 }
 
+void q1b() {
+    int row, column;
+    int **matrix;
+    float sum = 0;
+    
+    cout << "rows: ";
+    cin >> row;
+    cout << "columns: ";
+    cin >> column;
+
+    matrix = new int *[row];
+
+    for (int i = 0; i < row; i++) 
+        matrix[i] = new int[column];
+    
+    for (int j = 0; j < row; j++) {
+        for (int k = 0; k < column; k++) {
+            matrix[j][k] = rand() % 1000 + 1;
+            sum += matrix[j][k];
+        }
+    }
+
+    /*
+    prints array
+    for (int l = 0; l < row; l++) {
+        for (int m = 0; m < column; m++)
+            cout << matrix[l][m] << ' ';
+        cout << "\n";
+    }*/
+
+    cout << "average: " << sum / (row * column) << endl;
+
+    for (int m = 0; m < row; m++) {
+        delete[] matrix[m];
+    }
+
+    delete[] matrix;
+}
+
 int main() {
     q1a();
+    q1b();
     return 0;
 }
