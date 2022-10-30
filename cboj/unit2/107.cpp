4ef2515ab22152bc7cf9230178f6384b1d6a885d
Allison <allisoncretel@gmail.com>
2022-10-30 12:38:03 -0400

    fix init array

--- cboj/unit2/107.cpp
+++ cboj/unit2/107.cpp
@@ -16,9 +16,9 @@ int power(int base, int e) {
 
 //ex 2
 int fib (int n) {
-    if (n == 1)
+    if (n == 0)
         return 0;
-    else if (n == 2)
+    else if (n == 1)
         return 1;
     else 
         return fib(n - 1) + fib(n - 2);
